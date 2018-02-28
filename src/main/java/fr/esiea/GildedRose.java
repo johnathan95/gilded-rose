7c26d017867ee884fc69f20ab82852a4b3450fe7
Sanoyan <sanoyan.jeyakumar@gmail.com>
2018-02-28 16:50:43 -0500

    Refactoring v1

--- src/main/java/fr/esiea/GildedRose.java
+++ src/main/java/fr/esiea/GildedRose.java
@@ -8,55 +8,71 @@ public class GildedRose {
     }
 
     public void updateQuality() {
+
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+            String itemName = items[i].getName();
+            switch(itemName){
+                case "Aged Brie":
+                    items[i] = updateAgedBrie(items[i]);
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    items[i] = updateBackstage(items[i]);
+                    break;
+                case "Sulfuras, Hand of Ragnaros":
+                    // Rien à faire
+                    break;
+                default:
+                    items[i] = updateNonSpecificItem(items[i]);
+            }
+        }
+    }
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+    public Item updateAgedBrie(Item item){
+        if (item.getQuality() < 50) {
+            item.setQuality(item.getQuality() + 1);
+        }
+        item.setSellIn(item.getSellIn() - 1);
+        if (item.getSellIn() < 0) {
+            if (item.getQuality()< 50) {
+                item.setQuality(item.getQuality() + 1);
+            }
+        }
+        return item;
+    }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
+    public Item updateBackstage(Item item){
+        if (item.getQuality()< 50) {
+            item.setQuality(item.getQuality() + 1);
+            if (item.getSellIn() < 11) {
+                if (item.getQuality() < 50) {
+                    item.setQuality(item.getQuality() + 1);
                 }
             }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+            if (item.getSellIn() < 6) {
+                if (item.getQuality() < 50) {
+                    item.setQuality(item.getQuality() + 1);
                 }
             }
         }
+        item.setSellIn(item.getSellIn() - 1);
+
+        if (item.getSellIn() < 0) {
+            item.setQuality(0);
+        }
+        return item;
+    }
+
+    public Item updateNonSpecificItem(Item item){
+        if (item.getQuality() > 0) {
+            item.setQuality(item.getQuality() - 1);
+        }
+        item.setSellIn(item.getSellIn() - 1);
+
+        if (item.getSellIn() < 0) {
+            if (item.getQuality() > 0) {
+                item.setQuality(item.getQuality() - 1);
+            }
+        }
+        return item;
     }
 }
